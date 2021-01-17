4bfb055a8cdce474c78f0cf69bf6402adb664cd7
Smail KOURTA <gs_kourta@esi.dz>
2021-01-17 16:26:10 +0100

    added < and started adding rules for LT

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use egg::{rewrite as rw, *};
 use ordered_float::NotNan;
-use std::time::Instant;
+use std::{cmp::Ordering, time::Instant};
 
 pub type EGraph = egg::EGraph<Math, ConstantFold>;
 pub type Rewrite = egg::Rewrite<Math, ConstantFold>;
@@ -15,6 +15,7 @@ define_language! {
         "/" = Div([Id; 2]),
         "max" = Max([Id; 2]),
         "min" = Min([Id; 2]),
+        "<" = Lt([Id; 2]),
         Constant(Constant),
         Symbol(Symbol),
     }
@@ -42,6 +43,12 @@ impl Analysis<Math> for ConstantFold {
             Math::Div([a, b]) if x(b) != Some(0.0.into()) => x(a)? / x(b)?,
             Math::Max([a, b]) => std::cmp::max(x(a)?, x(b)?),
             Math::Min([a, b]) => std::cmp::min(x(a)?, x(b)?),
+            Math::Lt([a, b]) => NotNan::new(if x(a)?.cmp(&x(b)?) == Ordering::Less {
+                1.0
+            } else {
+                0.0
+            })
+            .unwrap(),
             _ => return None,
         })
     }
@@ -130,48 +137,22 @@ pub fn rules() -> Vec<Rewrite> { vec![
 
     rw!("distribute"; "(* ?a (+ ?b ?c))"        => "(+ (* ?a ?b) (* ?a ?c))"),
     rw!("factor"    ; "(+ (* ?a ?b) (* ?a ?c))" => "(* ?a (+ ?b ?c))"),
-    // rw!("max-min"; "* (max ?x ?b) (min ?x ?b)" => "(* ?x ?b)")
+    // rw!("mul-max-min"; "* (max ?a ?b) (min ?a ?b)" => "(* ?a ?b)")
 
-    // rw!("pow-mul"; "(* (pow ?a ?b) (pow ?a ?c))" => "(pow ?a (+ ?b ?c))"),
-    // rw!("pow0"; "(pow ?x 0)" => "1"
-    //     if is_not_zero("?x")),
-    // rw!("pow1"; "(pow ?x 1)" => "?x"),
-    // rw!("pow2"; "(pow ?x 2)" => "(* ?x ?x)"),
-    // rw!("pow-recip"; "(pow ?x -1)" => "(/ 1 ?x)"
-    //     if is_not_zero("?x")),
-    // rw!("recip-mul-div"; "(* ?x (/ 1 ?x))" => "1" if is_not_zero("?x")),
 
-    // rw!("d-variable"; "(d ?x ?x)" => "1" if is_sym("?x")),
-    // rw!("d-constant"; "(d ?x ?c)" => "0" if is_sym("?x") if is_const_or_distinct_var("?c", "?x")),
+    ///////////////////////// TESTS
 
-    // rw!("d-add"; "(d ?x (+ ?a ?b))" => "(+ (d ?x ?a) (d ?x ?b))"),
-    // rw!("d-mul"; "(d ?x (* ?a ?b))" => "(+ (* ?a (d ?x ?b)) (* ?b (d ?x ?a)))"),
+    rw!("div-to-mul"; "(/ ?x ?y)" => "(* ?x (/ 1 ?y))"),
 
-    // rw!("d-sin"; "(d ?x (sin ?x))" => "(cos ?x)"),
-    // rw!("d-cos"; "(d ?x (cos ?x))" => "(* -1 (sin ?x))"),
+    rw!("cancel-div-1surdiv"; "(* (/ 1 ?a) ?a)" => "1" if is_not_zero("?a")),
+    
+    rw!("cancel-mul-div"; "(/ (* ?y ?x) ?x)" => "?y"),
 
-    // rw!("d-ln"; "(d ?x (ln ?x))" => "(/ 1 ?x)" if is_not_zero("?x")),
 
-    // rw!("d-power";
-    //     "(d ?x (pow ?f ?g))" =>
-    //     "(* (pow ?f ?g)
-    //         (+ (* (d ?x ?f)
-    //               (/ ?g ?f))
-    //            (* (d ?x ?g)
-    //               (ln ?f))))"
-    //     if is_not_zero("?f")
-    //     if is_not_zero("?g")
-    // ),
 
-    // rw!("i-one"; "(i 1 ?x)" => "?x"),
-    // rw!("i-power-const"; "(i (pow ?x ?c) ?x)" =>
-    //     "(/ (pow ?x (+ ?c 1)) (+ ?c 1))" if is_const("?c")),
-    // rw!("i-cos"; "(i (cos ?x) ?x)" => "(sin ?x)"),
-    // rw!("i-sin"; "(i (sin ?x) ?x)" => "(* -1 (cos ?x))"),
-    // rw!("i-sum"; "(i (+ ?f ?g) ?x)" => "(+ (i ?f ?x) (i ?g ?x))"),
-    // rw!("i-dif"; "(i (- ?f ?g) ?x)" => "(- (i ?f ?x) (i ?g ?x))"),
-    // rw!("i-parts"; "(i (* ?a ?b) ?x)" =>
-    //     "(- (* ?a (i ?b ?x)) (i (* (d ?x ?a) (i ?b ?x)) ?x))"),
+    rw!("lt-x-xminus";  "(< (- ?a ?y) ?a )" => "1" ),
+    rw!("cancel-lt";  "(< ?a ?a)" => "0"),
+    rw!("cancel-max-lt";  "(< ?a ?a)" => "0"),
 ]}
 
 // egg::test_fn! {
@@ -272,7 +253,7 @@ pub fn rules() -> Vec<Rewrite> { vec![
 // }
 
 // egg::test_fn! {
-//     test_add, rules(), "(+ a (+ a (+ a (+ a (+ a a)))))" => "(* 6 a)"
+//     test_add, rules(), "(< 5 0)" => "(1)"
 // }
 
 // fn print_graph(egraph: &EGraph) {
@@ -283,32 +264,24 @@ pub fn rules() -> Vec<Rewrite> { vec![
 // }
 
 fn main() {
-    let start: RecExpr<Math> = "(max 2 3)".parse().unwrap();
-    let mut end: RecExpr<Math> = "10".parse().unwrap();
+    let start: RecExpr<Math> = "( < ( - v0 8 ) ( + ( * ( / ( - v0 v1 ) 9 ) 9 ) v1 ) )"
+        .parse()
+        .unwrap();
+    let end: Pattern<Math> = "(1)".parse().unwrap();
 
     let now = Instant::now();
     // That's it! We can run equality saturation now.
     let runner = Runner::default().with_expr(&start).run(rules().iter());
-    // print_graph(&runner.egraph);
     println!("Saturation took: {} ms", now.elapsed().as_millis());
-    let mut eclasses = runner.egraph.equivs(&start, &end);
-    if eclasses.is_empty() {
-        println!("{} and {} are not equivalent", start, end);
+
+    // print_graph(&runner.egraph);
+    let id = runner.egraph.find(*runner.roots.last().unwrap());
+    let matches = end.search_eclass(&runner.egraph, id);
+    if matches.is_none() {
+        println!("Could not prove goal:\n{}\n", end.pretty(40),);
     } else {
-        println!("{} and {} are equivalent", start, end);
+        println!("Proved goal:\n{}\n", end.pretty(40));
     }
-
-    end = "(* 3 x)".parse().unwrap();
-
-    eclasses = runner.egraph.equivs(&start, &end);
-    if eclasses.is_empty() {
-        println!("{} and {} are not equivalent", start, end);
-    } else {
-        println!("{} and {} are equivalent", start, end);
-    }
-
-    /// Graph Printing
-    // let egraph = &runner.egraph;
 
     // Extractors can take a user-defined cost function,
     // we'll use the egg-provided AstSize for now
@@ -318,7 +291,7 @@ fn main() {
     // We want to extract the best expression represented in the
     // same e-class as our initial expression, not from the whole e-graph.
     // Luckily the runner stores the eclass Id where we put the initial expression.
-    let (_, best_expr) = extractor.find_best(runner.egraph.find(*runner.roots.last().unwrap()));
+    let (_, best_expr) = extractor.find_best(id);
 
     println!(
         "Best Expr: {} found in {} ms (without saturation) {} ms (with saturation)",
