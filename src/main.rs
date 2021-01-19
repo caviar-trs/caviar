a01d65de497d461e1af8f0f199f9161450e5a43d
Smail KOURTA <gs_kourta@esi.dz>
2021-01-19 16:25:33 +0100

    added == rules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod trs;
 
 fn main() {
-    let start = "(&& (&& x (|| y z)) y)";
-    let end = "(&& x y)";
+    let start = "(== (+ x y) 0)";
+    let end = "(== x -y)";
     println!("Simplifying expression:\n {}\n", start);
     trs::prove_time(start, end);
 }
--- src/trs.rs
+++ src/trs.rs
@@ -328,9 +328,17 @@ fn rules() -> Vec<Rewrite> { vec![
 
     // Equality RULES
     rw!("comm-Eq";  "(== ?x ?y)"        => "(== ?y ?x)"),
+
+    rw!("other-side-Eq";  "(== (+ ?x ?y) ?z)"        => "(== ?x (- ?z ?y))"),
     // rw!("one-Eq";  "(== ?x 1)"        => "(?x)"),
     // rw!("zero-Eq";  "(== ?x 0)"        => "(! ?x)"),
     rw!("x-x-Eq";  "(== ?x ?x)"        => "1"),
+    // max(x, y) - y == 0
+    rw!("sub-let-max-Eq";  "(- (max ?x ?y) ?y)"        => "(<= ?x ?y)"),
+    rw!("sub-let-min-Eq";  "(- (min ?x ?y) ?y)"        => "(<= ?y ?x)"),
+
+    rw!("sub-let-max-Eq-1";  "(- ?y (max ?x ?y))"        => "(<= ?x ?y)"),
+    rw!("sub-let-min-Eq-1";  "(- ?y (min ?x ?y))"        => "(<= ?y ?x)"),
 
     // Inequality RULES
     rw!("comm-IEq";  "(!= ?x ?y)"      => "(!= ?y ?x)"),
