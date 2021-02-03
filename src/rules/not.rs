8910809f0c94bf6646ac8cf90e712b43825254f9
Smail KOURTA <gs_kourta@esi.dz>
2021-02-03 14:58:22 +0100

    running AND and OR

--- src/rules/not.rs
+++ src/rules/not.rs
@@ -5,12 +5,12 @@ pub type Rewrite = egg::Rewrite<Math, ConstantFold>;
 pub fn not() -> Vec<Rewrite> { vec![
     // NOT RULES
     rw!("cancel-eqlt";  "(<= ?x ?y)" => "(! (< ?y ?x))" ),
-    rw!("inv-cancel-eqlt";  "(! (< ?y ?x))" => "(<= ?x ?y)" ),
+    // rw!("inv-cancel-eqlt";  "(! (< ?y ?x))" => "(<= ?x ?y)" ),
     rw!("not-eqgt";  "(>= ?x ?y)" => "(! (< ?x ?y))" ),
-    rw!("not-eqgt-inv";  "(! (< ?x ?y))" => "(>= ?x ?y)" ),
+    // rw!("not-eqgt-inv";  "(! (< ?x ?y))" => "(>= ?x ?y)" ),
     rw!("not-eq";  "(! (== ?x ?y))" => "(!= ?x ?y)" ),
-    rw!("not-eq-inv";  "(!= ?x ?y)" => "(! (== ?x ?y))" ),
+    // rw!("not-eq-inv";  "(!= ?x ?y)" => "(! (== ?x ?y))" ),
     rw!("not-dif";  "(! (!= ?x ?y))" => "(== ?x ?y)" ),
     rw!("not-not-x";  "(! (! ?x))" => "?x" ),
-    rw!("not-dif-inv";  "(== ?x ?y)" => "(! (!= ?x ?y))" ),
+    // rw!("not-dif-inv";  "(== ?x ?y)" => "(! (!= ?x ?y))" ),
 ]}
