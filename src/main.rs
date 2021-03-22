9a7c43c6203c293d3f6cace90d9f6154b1a6eaf5
Smail KOURTA <gs_kourta@esi.dz>
2021-03-22 09:38:46 +0100

    fixed params for prove without iteration level

--- src/main.rs
+++ src/main.rs
@@ -23,14 +23,14 @@ fn prove_expressions(exprs_vect: &Vec<ExpressionStruct>,ruleset_class: i8, param
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let expressions = vec![
-        ("( <= ( - v0 11 ) ( + ( * ( / ( - v0 v1 ) 12 ) 12 ) v1 ) )","1"),
-        ("( <= ( + ( / ( - v0 v1 ) 8 ) 32 ) ( max ( / ( + ( - v0 v1 ) 257 ) 8 ) 0 ) )","1"),
-        ("( <= (/ a 2) (a))", "1"),
-        ("( <= ( min ( + ( * ( + v0 v1 ) 161 ) ( + ( min v2 v3 ) v4 ) ) v5 ) ( + ( * ( + v0 v1 ) 161 ) ( + v2 v4 ) ) )","1"),
-        ("( == (+ a b) (+ b a) )","1"),
-        ("( == (min a b) (a))","1"),
-    ];
+    // let expressions = vec![
+    //     ("( <= ( - v0 11 ) ( + ( * ( / ( - v0 v1 ) 12 ) 12 ) v1 ) )","1"),
+    //     ("( <= ( + ( / ( - v0 v1 ) 8 ) 32 ) ( max ( / ( + ( - v0 v1 ) 257 ) 8 ) 0 ) )","1"),
+    //     ("( <= (/ a 2) (a))", "1"),
+    //     ("( <= ( min ( + ( * ( + v0 v1 ) 161 ) ( + ( min v2 v3 ) v4 ) ) v5 ) ( + ( * ( + v0 v1 ) 161 ) ( + v2 v4 ) ) )","1"),
+    //     ("( == (+ a b) (+ b a) )","1"),
+    //     ("( == (min a b) (a))","1"),
+    // ];
     // generate_dataset(expressions,(30, 10000, 5), 2, 2);
     //generate_dataset_par(&expressions,(30, 10000, 5), 2, 10);
 
@@ -51,7 +51,7 @@ fn main() {
         let params = get_runner_params(1).unwrap();
         let (start, end) = get_start_end().unwrap();
         println!("Simplifying expression:\n {}\n to {}", start,end);
-        trs::prove_equiv(&start,&end, -1, params, false, true);
+        trs::prove(&start, -1, params, false, true);
         // trs::prove_expr(&start, &end, 2, true);
     }
 }
