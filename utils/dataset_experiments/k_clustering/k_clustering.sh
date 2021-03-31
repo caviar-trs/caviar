2fa1fad2a7eca212c6bd34b318be2a5c47fe0da0
Smail KOURTA <gs_kourta@esi.dz>
2021-03-31 16:46:31 +0100

    switched cargo run for the exec path in target: ready for lanka

--- utils/dataset_experiments/k_clustering/k_clustering.sh
+++ utils/dataset_experiments/k_clustering/k_clustering.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 K=$1
 python "`dirname "$0"`/k_clustering.py" $1
-RUST_BACKTRACE=1 cargo run --release test_classes results/expressions_egg.csv 1000 100000 3 "./results/k_"$K"_classes.json"
+target/release/egg_halides_trs test_classes results/expressions_egg.csv 1000 100000 3 "./results/k_"$K"_classes.json"
