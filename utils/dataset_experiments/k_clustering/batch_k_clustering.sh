361fb06e4a0d54989f1681caea58bbe3785c2448
Smail KOURTA <gs_kourta@esi.dz>
2021-04-08 08:28:54 +0100

    added script to run multiple time k_45 and k_111

--- utils/dataset_experiments/k_clustering/batch_k_clustering.sh
+++ utils/dataset_experiments/k_clustering/batch_k_clustering.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-../../../target/release/egg_halides_trs test_classes ../../../results/expressions_egg.csv 1000000 100000000 3 "./results/k_45_classes.json"
-../../../target/release/egg_halides_trs test_classes ../../../results/expressions_egg.csv 1000000 100000000 3 "./results/k_111_classes.json"
-python "`dirname "$0"`/analysis.py"
+for i in 1 2 3 4 5
+do
+   ../../../target/release/egg_halides_trs test_classes ../../../results/expressions_egg.csv 1000000 100000000 3 "./results/k_45_classes.json"
+    ../../../target/release/egg_halides_trs test_classes ../../../results/expressions_egg.csv 1000000 100000000 3 "./results/k_111_classes.json"
+done
 
