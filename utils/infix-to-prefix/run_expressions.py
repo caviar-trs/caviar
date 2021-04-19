4651bef881e123a84c135cdda5c795569cabbc37
adelnamani <ga_namani@esi.dz>
2021-04-19 09:28:26 +0100

    Dataset generation issues

--- utils/infix-to-prefix/run_expressions.py
+++ utils/infix-to-prefix/run_expressions.py
@@ -21,21 +21,24 @@ def extract(path, delimiter):
     return exprs
 
 def extract_one(i, row, remove):
-    next_expr = False
-    for tabou in remove:
-        if tabou in row[0]:
-            # print("=====", tabou)
-            next_expr = True
-    if next_expr:
-        # print("Skipped row :", i)
-        return None
-    row[0] = row[0].replace("(uint1)", "")
-    right = Expression(row[0])
-    expr = ' '.join(right.infixToPrefix())
-    expr = re.sub(
-        "\( \- (?P<var>[a-zA-Z_$][a-zA-Z_$0-9]*) \)", r'(* \1 -1)', expr)
-    print("Expression "+ str(i) +" processed.")
-    return expr
+    try:
+        next_expr = False
+        for tabou in remove:
+            if tabou in row[0]:
+                # print("=====", tabou)
+                next_expr = True
+        if next_expr:
+            # print("Skipped row :", i)
+            return None
+        row[0] = row[0].replace("(uint1)", "")
+        right = Expression(row[0])
+        expr = ' '.join(right.infixToPrefix())
+        expr = re.sub(
+            "\( \- (?P<var>[a-zA-Z_$][a-zA-Z_$0-9]*) \)", r'(* \1 -1)', expr)
+        print("Expression "+ str(i) +" processed.")
+        return expr
+    except:
+        print("Expression "+ str(i) +" skipped.")
 
 if __name__ == '__main__':
     if len(sys.argv) > 2:
